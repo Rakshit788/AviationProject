f7dc08ae875b0f5b7f4410e75bc47146cec6018d
Rakshit788 <rakshitchopra788@gmail.com>
2024-08-24 14:05:09 +0530

    starting server

---
+++ server/worker/app.js
@@ -0,0 +1,11 @@
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+import express from "express";
+import { config } from "dotenv";
+import userrouter from "./routes/user.routes.js";
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+
+
+
