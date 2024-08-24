78e75a54edee688c44c58e6972747e52821ae46a
Parthgogia <parthgogia19@gmail.com>
2024-08-24 14:39:43 +0530

    start server

--- server/worker/app.js
+++ server/worker/app.js
@@ -6,6 +6,9 @@ import userrouter from "./routes/user.routes.js";
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
-
-
+const app =express();
+const port =3000;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+})
 
